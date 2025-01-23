60ba810b3ce35dc348ebd911714927874c4fa7e1
Artur Novikov <artur.20177102@mail.ru>
2025-01-23 19:39:48 +0300

    get token

--- Genezis/src/components/ButtonTest.vue
+++ Genezis/src/components/ButtonTest.vue
@@ -1,11 +1,10 @@
-<script setup>
-import Loader from "./Loader.vue";
-</script>
-
 <template>
   <button
     class="mx-8 my-8 px-8 py-3 bg-blue-600 text-white font-semibold rounded-lg transition-all duration-300 ease-in-out transform hover:scale-105 active:scale-95 active:bg-blue-700"
   >
-    <Loader />
+    <div>Создать</div>
+    <!-- <Loader /> -->
   </button>
 </template>
+
+<script setup></script>
--- Genezis/src/views/MainView.vue
+++ Genezis/src/views/MainView.vue
@@ -1,6 +1,31 @@
 <script setup>
 import Button from "../components/ButtonTest.vue";
 import Selection from "../components/SelectionTest.vue";
+import axios from "axios";
+import { ref, onMounted } from "vue";
+
+const accessToken = ref(null);
+const clientId = "32185358";
+
+const fetchAccessToken = async () => {
+  try {
+    const response = await axios.get("/api/amocrm/test/oauth/get-token.php", {
+      headers: {
+        "X-Client-Id": clientId,
+        "Content-Type": "application/json",
+      },
+    });
+
+    accessToken.value = response.data.access_token;
+    console.log("Access Token:", accessToken.value);
+  } catch (error) {
+    console.error("Ошибка получения токена:", error);
+  }
+};
+
+onMounted(() => {
+  fetchAccessToken();
+});
 </script>
 <template>
   <div class="main"><Selection /><Button /></div>
